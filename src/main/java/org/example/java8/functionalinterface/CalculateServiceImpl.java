b992901b2feae6e4f3bfab1bbf965f78d15337db
Vijay Singh <vijay.singh@service.nsw.gov.au>
2023-06-02 10:59:02 +1000

    Java8: Functional interface example

--- src/main/java/org/example/java8/functionalinterface/CalculateServiceImpl.java
+++ src/main/java/org/example/java8/functionalinterface/CalculateServiceImpl.java
@@ -2,14 +2,12 @@ package org.example.java8.functionalinterface;
 
 public class CalculateServiceImpl {
     public int calculate(int a, int b, CalculateType type){
-        Calculator addition = (x, y) -> x+y;
-        Calculator subtract = (x, y) -> x-y;
-        Calculator multiply = (x, y) -> x*y;
+        Calculator calculator = (x, y) -> x+y;
         
         int result = switch(type){
-            case ADD -> addition.add(a,b);
-            case MULTIPLY -> multiply.multiply(a,b);
-            case SUBTRACT -> subtract.subtract(a,b);
+            case ADD -> calculator.add(a,b);
+            case MULTIPLY -> calculator.multiply(a,b);
+            case SUBTRACT -> calculator.subtract(a,b);
             default -> -1;
         };
         return result;
